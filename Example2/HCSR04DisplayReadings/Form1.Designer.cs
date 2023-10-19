f487f6f89ad7143424ef7cecb0ef2970e4427981
Art <artbeqiri17@gmail.com>
2023-10-19 22:43:45 +0200

    Added C# Gui Distance readings

---
+++ Example2/HCSR04DisplayReadings/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace HCSR04DisplayReadings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label_state = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this._baudrate = new System.Windows.Forms.ComboBox();
+            this.com_porta = new System.Windows.Forms.ComboBox();
+            this.shk_btn = new System.Windows.Forms.Button();
+            this.l_btn = new System.Windows.Forms.Button();
+            this.nmbus_panel = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.temp_reading_label = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.nmbus_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_state);
+            this.groupBox2.Controls.Add(this.label15);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this._baudrate);
+            this.groupBox2.Controls.Add(this.com_porta);
+            this.groupBox2.Controls.Add(this.shk_btn);
+            this.groupBox2.Controls.Add(this.l_btn);
+            this.groupBox2.Location = new System.Drawing.Point(12, 11);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(290, 170);
+            this.groupBox2.TabIndex = 27;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Modbus Settings";
+            // 
+            // label_state
+            // 
+            this.label_state.AutoSize = true;
+            this.label_state.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_state.Location = new System.Drawing.Point(120, 140);
+            this.label_state.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_state.Name = "label_state";
+            this.label_state.Size = new System.Drawing.Size(20, 25);
+            this.label_state.TabIndex = 26;
+            this.label_state.Text = "~";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(25, 138);
+            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(57, 18);
+            this.label15.TabIndex = 10;
+            this.label15.Text = "State :";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(53, 60);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(71, 17);
+            this.label13.TabIndex = 7;
+            this.label13.Text = "BaudRate";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(55, 25);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(62, 17);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "ComPort";
+            // 
+            // _baudrate
+            // 
+            this._baudrate.FormattingEnabled = true;
+            this._baudrate.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "115200"});
+            this._baudrate.Location = new System.Drawing.Point(133, 54);
+            this._baudrate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this._baudrate.Name = "_baudrate";
+            this._baudrate.Size = new System.Drawing.Size(108, 24);
+            this._baudrate.TabIndex = 3;
+            // 
+            // com_porta
+            // 
+            this.com_porta.FormattingEnabled = true;
+            this.com_porta.Location = new System.Drawing.Point(133, 22);
+            this.com_porta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.com_porta.Name = "com_porta";
+            this.com_porta.Size = new System.Drawing.Size(108, 24);
+            this.com_porta.TabIndex = 2;
+            this.com_porta.DropDown += new System.EventHandler(this.com_porta_DropDown);
+            // 
+            // shk_btn
+            // 
+            this.shk_btn.Enabled = false;
+            this.shk_btn.Location = new System.Drawing.Point(157, 94);
+            this.shk_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.shk_btn.Name = "shk_btn";
+            this.shk_btn.Size = new System.Drawing.Size(95, 25);
+            this.shk_btn.TabIndex = 1;
+            this.shk_btn.Text = "Disconnect";
+            this.shk_btn.UseVisualStyleBackColor = true;
+            this.shk_btn.Click += new System.EventHandler(this.shk_btn_Click);
+            // 
+            // l_btn
+            // 
+            this.l_btn.Location = new System.Drawing.Point(68, 94);
+            this.l_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.l_btn.Name = "l_btn";
+            this.l_btn.Size = new System.Drawing.Size(84, 25);
+            this.l_btn.TabIndex = 0;
+            this.l_btn.Text = "Connect";
+            this.l_btn.UseVisualStyleBackColor = true;
+            this.l_btn.Click += new System.EventHandler(this.l_btn_Click);
+            // 
+            // nmbus_panel
+            // 
+            this.nmbus_panel.Controls.Add(this.temp_reading_label);
+            this.nmbus_panel.Controls.Add(this.label1);
+            this.nmbus_panel.Enabled = false;
+            this.nmbus_panel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nmbus_panel.Location = new System.Drawing.Point(308, 12);
+            this.nmbus_panel.Name = "nmbus_panel";
+            this.nmbus_panel.Size = new System.Drawing.Size(804, 533);
+            this.nmbus_panel.TabIndex = 28;
+            this.nmbus_panel.TabStop = false;
+            this.nmbus_panel.Text = "NModbus4 Panel  Master";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 186);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(290, 359);
+            this.richTextBox1.TabIndex = 29;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Distance(cm): ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // temp_reading_label
+            // 
+            this.temp_reading_label.AutoSize = true;
+            this.temp_reading_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.temp_reading_label.ForeColor = System.Drawing.Color.Navy;
+            this.temp_reading_label.Location = new System.Drawing.Point(221, 84);
+            this.temp_reading_label.Name = "temp_reading_label";
+            this.temp_reading_label.Size = new System.Drawing.Size(0, 32);
+            this.temp_reading_label.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 548);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.nmbus_panel);
+            this.Controls.Add(this.groupBox2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.nmbus_panel.ResumeLayout(false);
+            this.nmbus_panel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label_state;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox _baudrate;
+        private System.Windows.Forms.ComboBox com_porta;
+        private System.Windows.Forms.Button shk_btn;
+        private System.Windows.Forms.Button l_btn;
+        private System.Windows.Forms.GroupBox nmbus_panel;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label temp_reading_label;
+    }
+}
+
---
+++ Example2/HCSR04DisplayReadings/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modbus.Device;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace HCSR04DisplayReadings
+{
+    public partial class Form1 : Form
+    {
+        SerialPort _port;
+        ModbusSerialMaster _master;
+  
+        public Form1()
+        {
+            InitializeComponent();
+            _baudrate.SelectedIndex = 1;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+
+        private void com_porta_DropDown(object sender, EventArgs e)
+        {
+            String[] portet = SerialPort.GetPortNames();//referenca per io.
+            com_porta.Items.Clear();
+            com_porta.Items.AddRange(portet);
+        }
+
+
+
+        private void l_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _port = new SerialPort(com_porta.Text, Convert.ToInt32(_baudrate.Text));
+                _port.ReadTimeout = 1000;
+                _port.WriteTimeout = 1000;
+                _port.Open();
+                label_state.Text = "Connected";
+                label_state.ForeColor = Color.Green;
+                nmbus_panel.Enabled = true;
+                _master = ModbusSerialMaster.CreateRtu(_port);
+                _master.Transport.ReadTimeout = 1300;
+                richTextBox1.AppendText("Connected to COM as Master");
+                shk_btn.Enabled = true;
+                l_btn.Enabled = false;
+                
+                timer1.Start();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Somthing Went wrong", "More Info:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                label_state.Text = "Disconected";
+                label_state.ForeColor = Color.Red;
+            }
+        }
+
+
+
+        private void shk_btn_Click(object sender, EventArgs e)
+        {
+            _master.Dispose();
+            _master = null;
+
+            // Destroy serial port
+            _port.Close();
+            _port.Dispose();
+            _port = null;
+            shk_btn.Enabled = false;
+            l_btn.Enabled = true;
+            nmbus_panel.Enabled = false;
+            richTextBox1.Clear();
+            timer1.Stop();
+        }
+      
+
+        
+      
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           ushort[] ESP32_Registers = _master.ReadHoldingRegisters(1,0,15);
+            temp_reading_label.Text = ESP32_Registers[0].ToString()+" cm";
+
+
+
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    
+
+
+}
+
